b7c85217aae9eca0ead8390703083b57ffada7d8
Clarice0997 <clarice09975@gmail.com>
2023-03-03 00:45:04 +0800

    Day 1 challenges solved

---
+++ Day 1/datatypes.js
@@ -0,0 +1,38 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// let str = 'String'
+// let isNot = true
+// let student
+// let index = null
+
+// console.log(typeof str)
+// console.log(typeof isNot)
+// console.log(typeof student)
+// console.log(typeof index)
+
+// 6. Declare four variables without assigning values
+// let name
+// let age
+// let sex
+// let birthday
+
+// 7. Declare four variables with assigned values
+// let x1 = 123
+// let x2 = 'str'
+// let x3 = 'arr'
+// let x4 = 'obj'
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// let firstName = 'Lin'
+// let lastName = 'Clarice'
+// let age = 20
+
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName = 'Lin',
+  lastName = 'Clarice',
+  age = 20
+
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 20
+let yourAge = 21
+
+console.log(`I am ${myAge} years old.\nYou are ${yourAge} years old.`)
---
+++ Day 1/variable.js
@@ -0,0 +1,10 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let str = 'String'
+let isNot = true
+let student
+let index = null
+
+console.log(typeof str)
+console.log(typeof isNot)
+console.log(typeof student)
+console.log(typeof index)
