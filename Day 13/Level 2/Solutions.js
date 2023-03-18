57dec2f12b869535e6d64b0b983d5ebe022cdbdf
Clarice0997 <clarice09975@gmail.com>
2023-03-18 23:50:11 +0800

    Day 13 Level 2 challenges solved

---
+++ Day 13/Level 2/Solutions.js
@@ -0,0 +1,8 @@
+// 1. 10 > 2 * 10 use console.assert()
+console.assert(10 > 2 * 10, '10 > 2 * 10 is incorrect')
+
+// 2. Write a warning message using console.warn()
+console.warn("It's a warning")
+
+// 3. Write an error message using console.error()
+console.warn("It's a error")
